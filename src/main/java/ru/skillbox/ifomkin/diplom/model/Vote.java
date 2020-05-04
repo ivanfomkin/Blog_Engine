9d981beb8f45ac73b315490cef8fcc63e3067a74
Ivan Fomkin <ivan-fomkin@yandex.ru>
2020-05-04 19:23:15 +0300

    Vote mapping has been added

--- src/main/java/ru/skillbox/ifomkin/diplom/model/Vote.java
+++ src/main/java/ru/skillbox/ifomkin/diplom/model/Vote.java
@@ -1,4 +1,31 @@
 package ru.skillbox.ifomkin.diplom.model;
 
-public class Vote {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "post_votes")
+public class Vote implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(name = "time", columnDefinition = "TIMESTAMP", nullable = false)
+    private LocalDateTime time;
+
+    //т.к. в postgres нет TINYINT, Будем использовать SMALLINT
+    @Column(name = "value", columnDefinition = "SMALLINT", nullable = false)
+    private Byte value;
 }
