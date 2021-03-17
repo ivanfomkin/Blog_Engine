50823bc6ce143777d1c7b02ebe5056bcdde6d1b4
Ivan Fomkin <ivan-fomkin@yandex.ru>
2021-03-17 13:09:11 +0300

    comment time bugFix

--- src/main/java/ru/skillbox/ifomkin/diplom/dto/comment/response/CommentInPostResponse.java
+++ src/main/java/ru/skillbox/ifomkin/diplom/dto/comment/response/CommentInPostResponse.java
@@ -1,5 +1,6 @@
 package ru.skillbox.ifomkin.diplom.dto.comment.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.skillbox.ifomkin.diplom.dto.user.UserInCommentResponse;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CommentInPostResponse {
     private int id;
+    @JsonProperty("timestamp")
     private long time;
     private String text;
     private UserInCommentResponse user;
