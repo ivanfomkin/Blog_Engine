a02f590fd86d4ff2c3f043865087db74b3c27ebf
Ivan Fomkin <ivan-fomkin@yandex.ru>
2021-03-18 14:51:06 +0300

    Autowired bugfix

--- src/main/java/ru/skillbox/ifomkin/diplom/config/StorageConfig.java
+++ src/main/java/ru/skillbox/ifomkin/diplom/config/StorageConfig.java
@@ -16,12 +16,16 @@ public class StorageConfig implements WebMvcConfigurer, CommandLineRunner {
     @Getter
     private String uploadPath;
 
+    private final StorageService storageService;
+
     @Autowired
-    StorageService storageService;
+    public StorageConfig(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler( "upload/**")
+        registry.addResourceHandler("upload/**")
                 .addResourceLocations("file:" + uploadPath + "/");
     }
 
